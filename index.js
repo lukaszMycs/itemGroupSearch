5cbfc5a887a29d1b32bacf79d98e657d9fcccf60
Damian Krolikowski <krolikowski.damian@gmail.com>
2018-09-30 08:30:46 +0200

    quick_push

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/,', (req, res) => {
+  res.json({ req, res });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express start listening on ${PORT}`);
+});
+
 const connectionModule = require('./src/connection');
 
 console.log('Welcome!');
