9e8a3227fb6ff69487d9b4ec5cbcfc959f0f08de
Damian Królikowski <krolikowski.damian@gmail.com>
2018-10-04 10:32:14 +0200

    first pages (#4)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,8 +10,10 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', (req, res) => {
+  res.render('default');
+});
+app.get('/status', (req, res) => {
   const results = {
     header: req.headers,
     host: req.host,
@@ -19,6 +22,10 @@ app.get('/', (req, res) => {
     query: req.query,
   };
   res.json(results);
+});
+
+app.get('/*', (req, res) => {
+  res.render('error');
 });
 
 app.listen(PORT, () => {
